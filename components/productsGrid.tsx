b13e2708e442824b3d53bc611c1a7322454c5c41
Jose Segura <josed.segurav@gmail.com>
2025-08-24 13:53:45 -0500

    fixed dark theme styles issues product grid

--- components/productsGrid.tsx
+++ components/productsGrid.tsx
@@ -102,7 +102,7 @@ export default function ProductsGrid({
             </section>
 
             {/* Products Grid */}
-            <section className="py-12">
+            <section className="py-12 bg-white">
 
                     <div className="container mx-auto px-4">
                         {filteredProducts.length > 0 ? (
