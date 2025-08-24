0b3210810cc24bf4144bec8ec351dcc77c3a7473
Jose Segura <josed.segurav@gmail.com>
2025-08-24 13:49:07 -0500

    fixed dark theme styles issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.className} antialiased`}>
+      <body className={`${geistSans.className} antialiased, text-black`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
